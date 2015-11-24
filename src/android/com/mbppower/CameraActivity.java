b3cc88781e2e6d23c44485732e9be7dc433b75e1
Peter Vojtek <peter.vojtek@gmail.com>
2015-11-24 14:47:19 +0100

    hotfix android camera low quality
    as discussed here:
    https://github.com/mbppower/CordovaCameraPreview/issues/53

--- src/android/com/mbppower/CameraActivity.java
+++ src/android/com/mbppower/CameraActivity.java
@@ -715,6 +715,13 @@ class Preview extends RelativeLayout implements SurfaceHolder.Callback {
             }
         }
 
+	// hotfix from https://github.com/mbppower/CordovaCameraPreview/issues/53
+        optimalSize = sizes.get(0);
+	for(int i=0;i<sizes.size();i++){
+	    if(sizes.get(i).width > optimalSize.width)
+	        optimalSize = sizes.get(i);
+	}
+
         Log.d(TAG, "optimal preview size: w: " + optimalSize.width + " h: " + optimalSize.height);
         return optimalSize;
     }
